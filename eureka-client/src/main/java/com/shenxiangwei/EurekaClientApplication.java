3e20226599c02fd77a362b2a3e7a7ee1b1d53828
shenxiangwei <1037323401@qq.com>
2019-09-01 17:07:02 +0800

    spring-cloud demo

---
+++ eureka-client/src/main/java/com/shenxiangwei/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.shenxiangwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * create by shenxiangwei on 2019/9/1 下午 4:56
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
---
+++ eureka-server/src/main/java/com/shenxiangwei/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.shenxiangwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * create by shenxiangwei on 2019/9/1 下午 4:44
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
